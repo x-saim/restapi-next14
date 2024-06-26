293ede658dbbf59cae24042af5ff3c77066b9335
x-saim <saimxamir@gmail.com>
2024-06-26 18:46:44 -0400

    Create db schema for Blog

---
+++ lib/models/blog.ts
@@ -0,0 +1,15 @@
+import { Schema, model, models } from 'mongoose';
+
+const BlogSchema = new Schema(
+  {
+    title: { type: 'string', required: true },
+    description: { type: 'string' },
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Blog = models.Blog || model('Blog', BlogSchema);
