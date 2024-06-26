e71494cf87c19d4086ac150b69441f049f8b076c
x-saim <saimxamir@gmail.com>
2024-06-26 18:59:33 -0400

    Create new GET route to get blogs given userId and categoryId

---
+++ app/api/(dashboard)/blogs/route.ts
@@ -0,0 +1,65 @@
+import { Category } from '@/lib/models/category';
+import { User } from '@/lib/models/user';
+import { Blog } from '@/lib/models/blog';
+import connect from '@/lib/db';
+import { NextResponse } from 'next/server';
+import { Types } from 'mongoose';
+
+export const GET = async (request: Request) => {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get('userId');
+    const categoryId = searchParams.get('categoryId');
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Invalid or missing userId' }),
+        { status: 400 }
+      );
+    }
+
+    if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Invalid or missing userId' }),
+        { status: 400 }
+      );
+    }
+
+    await connect();
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return new NextResponse(
+        JSON.stringify({ message: 'User not found in the database' }),
+        { status: 400 }
+      );
+    }
+
+    const category = await Category.findOne({ _id: categoryId, user: userId });
+    if (!category) {
+      return new NextResponse(
+        JSON.stringify({
+          message:
+            'Error: Either the Category not found or it does not belong to the user.',
+        }),
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const blogFilter: any = {
+      user: new Types.ObjectId(userId),
+      category: new Types.ObjectId(categoryId),
+    };
+
+    const userBlogs = await Blog.find(blogFilter);
+
+    return new NextResponse(JSON.stringify({ userBlogs }), { status: 200 });
+  } catch (err: any) {
+    return new NextResponse(`Error in fetching blogs: ${err.Message}`, {
+      status: 500,
+    });
+  }
+};
