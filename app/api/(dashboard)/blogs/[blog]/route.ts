43112bc41c1ab9b07e3c4b63a05952181028de63
x-saim <saimxamir@gmail.com>
2024-06-26 21:29:31 -0400

    Create a DELETE route for deleting a user's blog

--- app/api/(dashboard)/blogs/[blog]/route.ts
+++ app/api/(dashboard)/blogs/[blog]/route.ts
@@ -103,7 +103,6 @@ export const PATCH = async (request: Request, context: { params: any }) => {
     await connect();
 
     const user = await User.findById(userId);
-    //const category = await Category.findById(categoryId);
 
     if (!user) {
       return new NextResponse(
@@ -146,5 +145,58 @@ export const PATCH = async (request: Request, context: { params: any }) => {
         status: 500,
       }
     );
+  }
+};
+
+export const DELETE = async (request: Request, context: { params: any }) => {
+  const blogId = context.params.blog;
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get('userId');
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Invalid or missing userId' }),
+        { status: 400 }
+      );
+    }
+
+    if (!blogId || !Types.ObjectId.isValid(blogId)) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Invalid or missing blogId' }),
+        { status: 400 }
+      );
+    }
+
+    await connect();
+
+    // Retrieve the user and validate
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: 'User not found' }), {
+        status: 404,
+      });
+    }
+
+    //Retrieve the blog document object and validate
+    const blog = await Blog.findOne({ _id: blogId, user: userId });
+    if (!blog) {
+      return new NextResponse(JSON.stringify({ message: 'Blog not found' }), {
+        status: 404,
+      });
+    }
+
+    await Blog.findByIdAndDelete(blogId);
+
+    return new NextResponse(
+      JSON.stringify({ message: 'Blog was successfully deleted' }),
+      {
+        status: 200,
+      }
+    );
+  } catch (error: any) {
+    return new NextResponse('Error in deleting blog' + error.message, {
+      status: 500,
+    });
   }
 };
