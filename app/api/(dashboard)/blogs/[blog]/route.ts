a319ea5a9e1f213f2cb4cb3ed291c7f137c1e51a
x-saim <saimxamir@gmail.com>
2024-06-26 20:24:14 -0400

    Create GET request to fetch a single blog given its id

---
+++ app/api/(dashboard)/blogs/[blog]/route.ts
@@ -0,0 +1,85 @@
+import { Category } from '@/lib/models/category';
+import { User } from '@/lib/models/user';
+import { Blog } from '@/lib/models/blog';
+import connect from '@/lib/db';
+import { NextResponse } from 'next/server';
+import { Types } from 'mongoose';
+import { ObjectId } from 'mongodb';
+
+// GET: A Single Blog
+export const GET = async (request: Request, context: { params: any }) => {
+  const blogId = context.params.blog;
+
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get('userId');
+    const categoryId = searchParams.get('categoryId');
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Invalid or missing userId' }),
+        { status: 400 }
+      );
+    }
+
+    if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Invalid or missing categoryId.' }),
+        { status: 400 }
+      );
+    }
+
+    if (!blogId || !Types.ObjectId.isValid(blogId)) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Invalid or missing categoryId.' }),
+        { status: 400 }
+      );
+    }
+
+    await connect();
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: 'User not found' }), {
+        status: 404,
+      });
+    }
+
+    const category = await Category.findById(categoryId);
+
+    if (!category) {
+      return new NextResponse(
+        JSON.stringify({
+          message: 'Error: Category not found.',
+        }),
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const blog = await Blog.findOne({
+      _id: blogId,
+      category: categoryId,
+      user: userId,
+    });
+
+    if (!blog) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Blog was not found.' }),
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return new NextResponse(JSON.stringify({ blog }), {
+      status: 200,
+    });
+  } catch (error: any) {
+    return new NextResponse('Error in fetching blog data.' + error.message, {
+      status: 500,
+    });
+  }
+};
